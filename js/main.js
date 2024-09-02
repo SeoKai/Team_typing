d757b6a678337fa87aba39f76a97c01ce4bdc664
Seo Kai <ssw123c@gmail.com>
2024-09-03 01:53:57 +0900

    ,

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('.section-main').forEach((main) => {
+  const sliderContent = main.querySelector('.slider-container');
+  const leftButton = main.querySelector('.slider-btn.left');
+  const rightButton = main.querySelector('.slider-btn.right');
+
+  let scrollAmount = 0;
+
+  rightButton.addEventListener('click', () => {
+    const maxScroll = sliderContent.scrollWidth - sliderContent.clientWidth;
+    scrollAmount += 260;
+    if (scrollAmount > maxScroll) scrollAmount = maxScroll;
+    sliderContent.style.transform = `translateX(-${scrollAmount}px)`;
+  });
+
+  leftButton.addEventListener('click', () => {
+    scrollAmount -= 260;
+    if (scrollAmount < 0) scrollAmount = 0;
+    sliderContent.style.transform = `translateX(-${scrollAmount}px)`;
+  });
+});
