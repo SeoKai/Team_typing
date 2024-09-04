eb9bb61578c21ac1d35f7dad1c477d15cb2b2c1a
SeoKai <ssw123c@gmail.com>
2024-09-04 17:41:17 +0900

    .

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-document.querySelectorAll('.section-main').forEach((main) => {
-  const sliderContent = main.querySelector('.slider-container');
-  const leftButton = main.querySelector('.slider-btn.left');
-  const rightButton = main.querySelector('.slider-btn.right');
+document.querySelectorAll(".section-main").forEach((main) => {
+  const sliderContent = main.querySelector(".slider-container");
+  const leftButton = main.querySelector(".slider-btn.left");
+  const rightButton = main.querySelector(".slider-btn.right");
 
   let scrollAmount = 0;
 
-  rightButton.addEventListener('click', () => {
+  rightButton.addEventListener("click", () => {
     const maxScroll = sliderContent.scrollWidth - sliderContent.clientWidth;
     scrollAmount = maxScroll;
     if (scrollAmount > maxScroll) scrollAmount = maxScroll;
     sliderContent.style.transform = `translateX(-${scrollAmount}px)`;
   });
 
-  leftButton.addEventListener('click', () => {
+  leftButton.addEventListener("click", () => {
     scrollAmount = 0;
     if (scrollAmount < 0) scrollAmount = 0;
     sliderContent.style.transform = `translateX(-${scrollAmount}px)`;
