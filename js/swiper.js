f1870e8afb7a850e91aeb6d72bd89ce50f09ad59
SeoKai <ssw123c@gmail.com>
2024-09-06 17:51:40 +0900

    .

--- js/swiper.js
+++ js/swiper.js
@@ -1,24 +1,13 @@
-const swiper_01 = new Swiper(".swiper", {
-  //방향
-  direction: "horizontal",
-  //자동전환
-  // autoplay: {
-  //   delay: 10000,
-  //   pauseOnMouseEnter: true,
-  // },
-  //무한동작
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 20, // 슬라이드 간의 간격을 적절히 설정
   loop: true,
-  //페이징처리
   pagination: {
     el: ".swiper-pagination",
-    clickable: "true",
-    type: "bullets", //fraction, progressbar, bullets
+    clickable: true,
   },
-  //next,prev
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  // mousewheel: false,
-  effect: "scroll",
 });
